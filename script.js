c7502590adfb803469f81a57ead287c82d304194
guganravi06 <105877037+guganravi06@users.noreply.github.com>
2024-01-28 13:53:15 +0530

    Add files via upload
    unchanged files for deployment

---
+++ script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector('.container');
+const question = document.querySelector('.header_text');
+const yesBtn = document.querySelector('#yesButton');
+const image = document.querySelector('img');
+
+const containerRect = container.getBoundingClientRect();
+
+
+yesBtn.addEventListener('click',()=>{
+    question.innerHTML='waiting for the day';
+}); 
+yesBtn.addEventListener('click',()=>{
+    image.innerHTML="yes.gif";
+});
+
+function sendSMS() {
+    // Make an HTTP POST request to your local server
+    $.post('http://localhost:3000/send-sms', function (data) {
+        console.log(data);
+        alert('SMS sent successfully!');
+
+        document.getElementById('dynamicImage').src = 'yes.gif'; 
+    });
+}
+
+function moveButton() {
+    var x = Math.random() * (window.innerWidth - 
+        document.getElementById('noButton').offsetWidth - 85);
+    var y = Math.random() * (window.innerHeight - 
+        document.getElementById('noButton').offsetHeight - 48);
+    document.getElementById('noButton').style.left = x + 'px';
+    document.getElementById('noButton').style.top = y + 'px';
+}
